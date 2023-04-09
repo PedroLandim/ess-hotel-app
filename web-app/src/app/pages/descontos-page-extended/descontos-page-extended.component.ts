054dd488d53446f4e989a515ea8cf2e445371bd9
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2023-04-09 13:35:02 -0300

    Atualizando Branch

--- web-app/src/app/pages/descontos-page-extended/descontos-page-extended.component.ts
+++ web-app/src/app/pages/descontos-page-extended/descontos-page-extended.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-descontos-page-extended',
   templateUrl: './descontos-page-extended.component.html',
   styleUrls: ['./descontos-page-extended.component.scss']
 })
+
 export class DescontosPageExtendedComponent {
   @Input() hotelName!: string;
   @Input() location!: string;
